4b81f16030a1334ca245a081c0fdc1c1bfa95aaf
oricalvo <ori.calvo@gmail.com>
2015-09-02 13:56:26 +0300

    More

---
+++ Labs/01 JavaScript Pitfalls/Test.js
@@ -0,0 +1,12 @@
+(function () {
+
+    var sb = StringBuilder();
+
+    for (var i = 0; i < 10; i++) {
+        ab.append("Line " + i + "\n");
+    }
+
+    var str = sb.get();
+
+    console.log(str);
+})();
